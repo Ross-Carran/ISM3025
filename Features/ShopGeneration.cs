368144e7b71930ecf10f321898a1edd47d369dfe
Bradley Burcar <btburcar@gmail.com>
2019-07-18 03:32:50 -0400

    Added all shops.

--- Features/ShopGeneration.cs
+++ Features/ShopGeneration.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using BattleTech;
 using Harmony;
+using System.Linq;
 
 namespace ISM3025.Features
 {
@@ -13,6 +14,13 @@ namespace ISM3025.Features
             if (!string.IsNullOrEmpty(enableTag) && !def.Tags.Contains(enableTag))
                 return;
 
+            // add Black Market. Even "abandoned" systems can have Black Markets.
+            if (def.BlackMarketShopItems == null)
+                Traverse.Create(def).Property("BlackMarketShopItems").SetValue(new List<string>());
+
+            if (def.Tags.Contains("planet_other_blackmarket"))
+                def.BlackMarketShopItems.Add("itemCollection_faction_AuriganPirates");
+
             // if a system has one of these tags, we don't want to add stuff
             foreach (var ignoreTag in Main.Settings.GenerateShopsIgnoreTags)
             {
@@ -20,6 +28,18 @@ namespace ISM3025.Features
                     return;
             }
 
+            // if a system is a faction HQ, add the faction shop
+            if (def.FactionShopItems == null)
+                Traverse.Create(def).Property("FactionShopItems").SetValue(new List<string>());
+
+            if (def.Tags.Any(x => x.StartsWith("planet_other_factionhq")) && def.Owner != Faction.ComStar)
+            {
+                Traverse.Create(def).Property("FactionShopOwner").SetValue(def.Owner);
+                string FactionStoreName = "itemCollection_faction_" + def.Owner.ToString();
+                def.FactionShopItems.Add(FactionStoreName);
+            }
+
+            //add the system shop items
             if (def.SystemShopItems == null)
                 Traverse.Create(def).Property("SystemShopItems").SetValue(new List<string>());
 
@@ -28,6 +48,9 @@ namespace ISM3025.Features
             {
                 if (!def.Tags.Contains(tag))
                     continue;
+
+                if (tag.StartsWith("planet_faction") && (def.Tags.Contains("planet_other_megacity") || def.Tags.Contains("planet_pop_large")))
+                    tag.Replace("minor", "major");
 
                 def.SystemShopItems?.Add(Main.Settings.TagToShopItemCollection[tag]);
             }
